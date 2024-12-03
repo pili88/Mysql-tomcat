26760c5f5aa0e97196b717cb50004fa918a5e626
Christopher Nunez <elgary1129@gmail.com>
2024-12-03 19:42:20 -0300

    cambios para que funcione el proyecto

--- src/main/java/com/alumnos/DatabaseConnection.java
+++ src/main/java/com/alumnos/DatabaseConnection.java
@@ -9,10 +9,10 @@ public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
         try {
             // Cargar el driver JDBC
-            Class.forName("org.postgresql.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             // Establecer la conexión
             return DriverManager.getConnection(
-                    "jdbc:postgresql:/localhost/algo", "postgres", "Julietita12"); // Cambia las credenciales
+                    "jdbc:mysql://localhost:3306/usuarios_db", "root", "bootcamp0084"); // Cambia las credenciales
         } catch (ClassNotFoundException e) {
             throw new SQLException("Driver no encontrado", e);
         }
